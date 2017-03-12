fab573fcd97211e1a9725636f15ed24c88fffdf4
Nikolai Desktop <StoneCat@luis.is>
2017-03-12 14:04:06 +0100

    sample data generator to grafana query,add /search

---
+++ src/main/java/com/DCC/skarf/api/Helper/DataGenerator/GrafanaQueryDataGenerator.java
@@ -0,0 +1,45 @@
+package com.DCC.skarf.api.Helper.DataGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.DCC.skarf.api.Grafana.Request.DataObjects.Query.GrafanaQueryRequest;
+import com.DCC.skarf.api.Grafana.Request.DataObjects.Query.GrafanaQueryTargets;
+import com.DCC.skarf.api.Grafana.Response.DataObjects.Query.GrafanaQueryResponse;
+
+public class GrafanaQueryDataGenerator {
+	
+	private List<GrafanaQueryResponse> responseList = new ArrayList<GrafanaQueryResponse>();
+	
+	public GrafanaQueryDataGenerator() {
+		//Nothing
+	}
+	
+	public GrafanaQueryDataGenerator(GrafanaQueryRequest input) {
+		
+		for (GrafanaQueryTargets target : input.getTargets()) {
+			GrafanaQueryResponse oneResponse = new GrafanaQueryResponse();
+			//Get target name as requested
+			oneResponse.setTarget(target.getTarget());
+			
+			//Get sample DataPoint amount as requested
+			long baseTime = System.currentTimeMillis();
+			List<long[]> datapoints = new ArrayList<long[]>();
+			for (int i = 1; i <= input.getMaxDataPoints(); i++) {
+				datapoints.add(new long[] {(long) ThreadLocalRandom.current().nextInt(0, 301), baseTime});
+				baseTime = baseTime - 1000;
+			}
+			oneResponse.setDatapoints(datapoints);
+			
+			//Adding the response to ArrayList for output
+			this.responseList.add(oneResponse);
+		}
+				
+	}
+	
+	public List<GrafanaQueryResponse> getResponseList() {
+		return this.responseList;
+	}
+
+}
--- src/main/java/com/DCC/skarf/api/URLS/GrafanaSearch.java
+++ src/main/java/com/DCC/skarf/api/URLS/GrafanaSearch.java
@@ -1,24 +1,21 @@
 package com.DCC.skarf.api.URLS;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.DCC.skarf.api.Grafana.Request.DataObjects.Query.GrafanaQueryRequest;
-import com.DCC.skarf.api.Grafana.Response.DataObjects.Query.GrafanaQueryResponse;
-import com.DCC.skarf.api.Helper.DataGenerator.GrafanaQueryDataGenerator;
+import com.DCC.skarf.api.Grafana.Request.DataObjects.Search.GrafanaSearchRequest;
+import com.DCC.skarf.api.Grafana.Response.DataObjects.Search.GrafanaSearchResponse;
 
 @Controller
-@RequestMapping("/query")
-public class GrafanaQuery {
+@RequestMapping("/search")
+public class GrafanaSearch {
 	
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json; charset=UTF-8", consumes = "application/json; charset=UTF-8")
-	public @ResponseBody List<GrafanaQueryResponse> runQuery(@RequestBody GrafanaQueryRequest jsonString) {
-		return new GrafanaQueryDataGenerator(jsonString).getResponseList();
+	public @ResponseBody String[] runQuery(@RequestBody GrafanaSearchRequest jsonString) {
+		return new GrafanaSearchResponse().getDatapoints();
 	}
 
 }
