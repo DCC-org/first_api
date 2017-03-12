d0d55341e69eebeb6e887647345b6c0c7ffeec20
Nikolai Desktop <StoneCat@luis.is>
2017-03-12 15:39:00 +0100

    Replace text object with normal string

--- src/main/java/com/DCC/skarf/api/URLS/ConnectionTest.java
+++ src/main/java/com/DCC/skarf/api/URLS/ConnectionTest.java
@@ -5,15 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.DCC.skarf.api.DataObjects.Text;
-
 @Controller
 @RequestMapping("/")
 public class ConnectionTest {
 
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
-	public @ResponseBody Text testConnection() {
-		return new Text("Don't follow dreams, hunt goals");
+	public @ResponseBody String testConnection() {
+		return "Don't follow dreams, hunt goals";
 	}
 
 	
