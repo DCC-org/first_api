aa047c10f779855907cba6513ba27a94bb911b7a
Nikolai Desktop <StoneCat@luis.is>
2017-03-12 21:57:54 +0100

    Fix response bug in connection test object

--- src/main/java/com/DCC/skarf/api/URLS/ConnectionTest.java
+++ src/main/java/com/DCC/skarf/api/URLS/ConnectionTest.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.DCC.skarf.api.DataObjects.ApiResponse;
+
 @Controller
 @RequestMapping("/")
 public class ConnectionTest {
 
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
-	public @ResponseBody String testConnection() {
-		return "Don't follow dreams, hunt goals";
+	public @ResponseBody ApiResponse testConnection() {
+		return new ApiResponse(4, "Don't follow dreams, hunt goals");
 	}
 
 	
