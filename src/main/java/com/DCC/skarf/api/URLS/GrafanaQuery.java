6b166d5dcc806e7dcd10b7e640981636a89c2ee4
Nikolai Desktop <StoneCat@luis.is>
2017-05-07 18:04:13 +0200

    Get org_id from header test/pseudo code

--- src/main/java/com/DCC/skarf/api/URLS/GrafanaQuery.java
+++ src/main/java/com/DCC/skarf/api/URLS/GrafanaQuery.java
@@ -1,12 +1,10 @@
 package com.DCC.skarf.api.URLS;
 
-import java.util.Enumeration;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,13 +20,8 @@ import io.swagger.util.Json;
 public class GrafanaQuery {
 
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json; charset=UTF-8", consumes = "application/json; charset=UTF-8")
-	public @ResponseBody List<GrafanaQueryResponse> runQuery(@RequestBody GrafanaQueryRequest jsonString, HttpServletRequest request) {
-        Enumeration headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String key = (String) headerNames.nextElement();
-            String value = request.getHeader(key);
-            System.out.println(key + " = " + value);
-        }
+	public @ResponseBody List<GrafanaQueryResponse> runQuery(@RequestBody GrafanaQueryRequest jsonString, @RequestHeader(value="referer") String org_id) {
+        System.out.println(org_id);
 		System.out.println(Json.pretty(jsonString));
 		return new GrafanaQueryDataGenerator(jsonString).getResponseList();
 	}
