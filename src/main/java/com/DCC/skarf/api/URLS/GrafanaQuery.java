46b6f16e4bdee6602689eea51cd99609eeafc8fa
Nikolai Desktop <StoneCat@luis.is>
2017-05-07 17:54:06 +0200

    Try to get grafana header infos

--- src/main/java/com/DCC/skarf/api/URLS/GrafanaQuery.java
+++ src/main/java/com/DCC/skarf/api/URLS/GrafanaQuery.java
@@ -1,6 +1,9 @@
 package com.DCC.skarf.api.URLS;
 
+import java.util.Enumeration;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +22,13 @@ import io.swagger.util.Json;
 public class GrafanaQuery {
 
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json; charset=UTF-8", consumes = "application/json; charset=UTF-8")
-	public @ResponseBody List<GrafanaQueryResponse> runQuery(@RequestBody GrafanaQueryRequest jsonString) {
+	public @ResponseBody List<GrafanaQueryResponse> runQuery(@RequestBody GrafanaQueryRequest jsonString, HttpServletRequest request) {
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            System.out.println(key + " = " + value);
+        }
 		System.out.println(Json.pretty(jsonString));
 		return new GrafanaQueryDataGenerator(jsonString).getResponseList();
 	}
